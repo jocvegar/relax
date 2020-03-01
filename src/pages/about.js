9dd945ee43f158ba280b14a44c0cb9b3e3a0ee56
Jose <jocvegar@gmail.com>
2020-03-01 12:04:28 -0600

    Improve about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,9 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { makeStyles } from "@material-ui/core/styles"
-import color from "@material-ui/core/colors/amber"
+import grey from "@material-ui/core/colors/grey"
+import Avatar from "@material-ui/core/Avatar"
+import Typography from "@material-ui/core/Typography"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -10,16 +12,40 @@ const useStyles = makeStyles(theme => ({
     position: "absolute",
     left: 0,
     right: 0,
-    backgroundColor: "#F5F8FA",
+    backgroundColor: grey[100],
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    alignItems: "center",
+    alignContent: "stretch",
+    paddingTop: 50,
+  },
+
+  largeAvatar: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
   },
 }))
 
 const About = () => {
   const classes = useStyles()
+
   return (
     <Layout>
       <SEO title="Count down" />
-      <div className={classes.root}>hola</div>
+      <div className={classes.root}>
+        <div className={classes.container}>
+          <Avatar
+            alt="Jose V"
+            src="https://s.gravatar.com/avatar/359fbf62c0b92f8c6c063b35b92ea280?s=200"
+            className={classes.largeAvatar}
+          />
+          <Typography variant="h3">Jose Vega</Typography>
+        </div>
+      </div>
     </Layout>
   )
 }
